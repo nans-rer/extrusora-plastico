18843fea1cf8aa7c8d59d16f35ff08dbee4cd39e
nans-rer <hernanreyesaldi@gmail.com>
2025-01-27 18:59:11 -0300

    add working retraction

--- stepper-motor/stepper-motor.ino
+++ stepper-motor/stepper-motor.ino
@@ -1,38 +1,52 @@
 #include <AccelStepper.h>
+long currentMillis; // Current milliseconds
+long retractionMillis; // Retraction milliseconds
+long retractionTime = 600; // Time of retraction
 int STEP = 4; // Pulse pin
 int DIR  = 5; // Direction pin
 int ENA = 6; // Enable pin
 int BUTTON = 2; // Button pin
-long RPM = 300; 
+long RPM = 800; 
 long SPR = 400; // Steps per revolution
-long SPS; // Steps per second, se entrega en setSpeed()
+long SPS; // Steps per second, used in setSpeed()
+bool counter; // Counter used for changing direction
 AccelStepper stepper(1, STEP, DIR); // Construct a stepper in driver mode (1)
 
 void setup()
-{ 
-  pinMode(BUTTON, INPUT_PULLUP);
+{  
   stepper.setEnablePin(ENA);
   stepper.setPinsInverted(0, 0, 1);
   stepper.enableOutputs();
 
-  stepper.setMaxSpeed(10000);	
-  stepper.setAcceleration(1000);
+  stepper.setAcceleration(SPS/3);
   SPS = ((RPM * SPR) / 60);
+  stepper.setMaxSpeed(SPS);	
   stepper.setSpeed(SPS);
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON), backStep, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), retraction, FALLING);
+  counter = 1;
 }
 
 void loop()
-{  
-  int buttonVal = digitalRead(BUTTON);
-  if (buttonVal == LOW) {
+{ 
+  currentMillis = millis(); 
+  if ((currentMillis - retractionMillis) <= retractionTime && digitalRead(BUTTON)==LOW){
+    stepper.runSpeed();
+  }
+  else {
+  if (counter == 0) {
+    stepper.setSpeed(SPS);
+    counter = 1;
+    }
+  }
+  while (digitalRead(BUTTON)==HIGH){
+    currentMillis = millis();
     stepper.runSpeed();
   }
 }
 
-void backStep() {
-  stepper.stop();
-  stepper.setCurrentPosition(0);
-  stepper.runToNewPosition(-(SPR/2));
+void retraction() {
+  retractionMillis = currentMillis;
+  stepper.setSpeed(-SPS);
+  counter = 0;
 }
